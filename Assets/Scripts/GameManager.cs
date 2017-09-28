743109b7673e4d7414a73452ca50f193ff69f240
Dan Gorman <danthemangorman@gmail.com>
2017-09-28 13:35:29 -0400

    added Unity project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager INSTANCE;
+
+	// Use this for initialization
+	void Start () {
+		if (INSTANCE != null) {
+            this.enabled = false;
+            return;
+        }
+        INSTANCE = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
